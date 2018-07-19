1b51d64a38db35a2a5a099ed873e953a45acf859
Khaeshah <sergi.sorigue@gmail.com>
2018-07-19 02:52:57 +0200

    refactor

--- main.py
+++ main.py
@@ -5,26 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # At start we get the latest comic
-    latestComic = xkcd.getLatestComic()
-    # Parse the comic to HTML
-    return parseComicHTML(latestComic)
+    return showComicHTML(xkcd.getLatestComic())
+
 
 @app.route('/<int:comicId>')
 def show_post(comicId):
 
     # show the comic with the given id, the id is an integer
-    comic = xkcd.getComic(comicId)
+    return showComicHTML(xkcd.getComic(comicId), comicId)
 
-    # Parse the comic to HTML
-    return parseComicHTML(comic, comicId)
 
 @app.route('/random')
 def randomComic():
-    # show a random comic
-    randomComic = xkcd.getRandomComic()
-    return parseComicHTML(randomComic);
 
-def parseComicHTML(comic, comicId = -1):
+    # show a random comic
+    return showComicHTML(xkcd.getRandomComic());
+
+
+def showComicHTML(comic, comicId = -1):
     try:
         title = comic.getTitle()
         imageUrl = comic.getImageLink()
@@ -36,5 +34,5 @@ def parseComicHTML(comic, comicId = -1):
 
     except Exception as e:
         # If comic does not exist the latest is returned
-        return parseComicHTML(xkcd.getLatestComic())
+        return showComicHTML(xkcd.getLatestComic())
     return render_template('showComic.html',title = title, sourceUrl = sourceUrl, imageUrl = imageUrl, comicId = comicId)
